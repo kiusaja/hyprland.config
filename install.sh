4493fd46e1a377bb00e6d52431eeff22c0d88865
kiusaja <337431@gmail.com>
2026-02-25 14:45:32 +0100

    Install Skript und Verlegung der Ordner

---
+++ install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Skript zum installieren der Symlinks auf fürs Sachen wie Waybar und co.
+
+DOT_DIR="$HOME/kiusaja"
+CONFIG_DIR="$HOME/.config"
+
+echo "beep boop Symlinks hihi:..."
+
+cd "$DOT_DIR" || { echo "Verzeichnis $DOT_DIR wurde nicht gefunden, tja!"; exit 1; }
+
+for item in *; do
+    if [[ "$item" == "hypr" || "$item" == "install.sh" ]]; then
+        echo "SKIPPPPPPP!!!1!!!: $item"
+        continue
+    fi
+
+    ln -snf "$DOT_DIR/$item" "$CONFIG_DIR/$item"
+    
+    echo "Easy peasy geschafft: $item -> $CONFIG_DIR/$item"
+done
+
+echo "Jo buddy du hast es geschafft"
